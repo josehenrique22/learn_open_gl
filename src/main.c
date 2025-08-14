c44291b4b64835c268c63737b3219bceb9721a8a
joser <joserbtgt@gmail.com>
2025-08-14 14:21:16 -0300

    color windows

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "glad.h"
 #include "glfw3.h"
 #include <stdio.h>
+#include <stdbool.h>
 
 typedef struct {
     int height;
@@ -8,18 +9,23 @@ typedef struct {
     char *title;
 } window_config;
 
+void procress_input(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
 int main(void) {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
     window_config w_config = {600, 800, "Learn OpenGL"};
 
     GLFWwindow* window = glfwCreateWindow(w_config.width, w_config.height, 
     w_config.title, NULL, NULL);
 
-    if (window == NULL) {
+    if (window == NULL) {       
         printf("Fail to create GLFW Windows\n");
         glfwTerminate();
 
@@ -35,7 +41,16 @@ int main(void) {
 
     glViewport(0, 0, w_config.width, w_config.height);
 
+    // Render loop
     while (!glfwWindowShouldClose(window)) {
+        // Input  
+        procress_input(window);
+
+        // rendering commands
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Check and call events and swap the buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
